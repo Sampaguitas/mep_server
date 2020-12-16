c51db2457338f85cad1fa346cc52cdb51eba43cf
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-12-16 13:19:01 +0400

    changes

--- routes/dropbox/length.js
+++ routes/dropbox/length.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const pff_type = decodeURI(req.query.pff_type);
     res.status(200).json(require('../../constants/lengths.json')
-    .filter(e.pffTypes.includes(pff_type))
+    .filter(e => e.pffTypes.includes(pff_type))
     .reduce(function (acc, cur) {
         acc.push(cur.name);
         return acc;
